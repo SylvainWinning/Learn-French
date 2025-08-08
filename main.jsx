a487a02428c9dc943b13bb4f712c870e6b3d8a3e
SylvainWinning <168933918+SylvainWinning@users.noreply.github.com>
2025-08-08 23:10:15 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app_react_style_apple_learn_french.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
